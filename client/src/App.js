315918e1e541f03e52bfcbb547830ae35fda68b8
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2019-02-19 11:00:57 -0500

    adding auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { user: null };
+  }
+
+  componentDidMount() {
+    fetch('/api/user', { credentials: 'same-origin' })
+      .then(res => res.json())
+      .then((res) => {
+        this.setState({ user: res.user });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="App-intro" style={{width: '500px', margin:'0 auto'}}>
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+          <p><a href="/">back to express server</a></p>
+          <pre style={{textAlign: "left", background: "#eee"}}>{JSON.stringify(this.state.user, null, 2)}</pre>
+        </div>
       </div>
     );
   }

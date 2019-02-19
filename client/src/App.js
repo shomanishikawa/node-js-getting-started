d67478fd35e4729a84989f8369b8f6246e29bd8d
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2019-02-19 16:05:40 -0500

    adding airtable api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import Airtable from 'airtable';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { user: null };
+    this.state = {
+      user: null,
+      airtable: null,
+    };
+
+    this.base = new Airtable({apiKey: 'keykMFBqNhPAlAjvp'}).base('appzxqfrpXeu0ZN9p');
+
+    this.base('main').select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 9999,
+      view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+      records.forEach(function(record) {
+        console.log(record);
+        // console.log('Retrieved', record.get('name'));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      // fetchNextPage();
+
+    }, function done(err) {
+      if (err) { console.error(err); return; }
+    });
   }
 
   componentDidMount() {

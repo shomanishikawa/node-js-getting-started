d1a86901eb54f79e61bbb4fb64eca1cae0fc7e60
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2019-02-20 18:15:46 -0500

    integrate with questions from airtable

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,76 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Airtable from 'airtable';
+import _ from 'lodash';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import logo from './logo.svg';
 import './App.css';
+import styles from './styles';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: { main: '#084B89' },
+    secondary: { main: '#FAAF48' },
+  },
+  typography: {
+    fontFamily: ['Lato', 'sans-serif'].join(','),
+    body2: {
+      fontSize: 12,
+    },
+    title: {
+      fontSize: 14,
+    }
+  },
+  overrides: {
+    MuiCard: {
+      root: {
+        color: 'black',
+      },
+    },
+  },
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       user: null,
-      airtable: null,
+      value: 'female',
+      questions: [],
+      answers: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+      loaded: false,
     };
 
     this.base = new Airtable({apiKey: 'keykMFBqNhPAlAjvp'}).base('appzxqfrpXeu0ZN9p');
 
     this.base('main').select({
-      // Selecting the first 3 records in Grid view:
       maxRecords: 9999,
       view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
-      records.forEach(function(record) {
-        console.log(record);
-        // console.log('Retrieved', record.get('name'));
-      });
+    }).eachPage((records, fetchNextPage) => {
+      this.setState({loaded: true, questions: _.map(records, (record, i) => {
+        return {
+          name: record.get('name'),
+          options: Object.entries(record.fields).map(([key,value]) => {
+            if (key === 'name') {
+              return null;
+            }
 
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      // fetchNextPage();
-
-    }, function done(err) {
+            return {
+              letter: key,
+              value,
+            };
+          })
+        }
+      })})
+    }, err => {
       if (err) { console.error(err); return; }
     });
   }
@@ -42,21 +83,69 @@ class App extends Component {
       });
   }
 
+  handleChange = event => {
+    const theAnswers = this.state.answers;
+    theAnswers[event.target.name] = event.target.value;
+    this.setState({ answers: theAnswers });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { questions, loaded } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro" style={{width: '500px', margin:'0 auto'}}>
-          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
-          <p><a href="/">back to express server</a></p>
-          <pre style={{textAlign: "left", background: "#eee"}}>{JSON.stringify(this.state.user, null, 2)}</pre>
+      <MuiThemeProvider theme={muiTheme}>
+        <div className="App">
+          <CssBaseline />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={6}>
+              <div className="App-intro" style={{width: '500px', margin:'0 auto'}}>
+                <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+                <p><a href="/">back to express server</a></p>
+                <pre style={{textAlign: "left", background: "#eee"}}>{JSON.stringify(this.state.user, null, 2)}</pre>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {loaded && (
+                <div className={classes.formRoot}>
+                  <FormControl component="fieldset" className={classes.formControl}>
+                    {questions.map((question, i) => (
+                      <div key={i}>
+                        <FormLabel component="legend">{question.name}</FormLabel>
+                        <RadioGroup
+                          aria-label={question.name}
+                          name={i}
+                          className={classes.formGroup}
+                          value={this.state.answers[i]}
+                          onChange={this.handleChange}
+                        >
+                          {question.options.map((option, j) => {
+                            if (option) {
+                              return (<FormControlLabel key={j} value={option.letter} control={<Radio />} label={option.value} />)
+                            }
+                            return null;
+                          })}
+                        </RadioGroup>
+                      </div>
+                    ))}
+                  </FormControl>
+                </div>
+              )}
+
+            </Grid>
+          </Grid>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
---
+++ client/src/styles.js
@@ -0,0 +1,158 @@
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'appBarShift-left': {
+    marginLeft: drawerWidth,
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  'content-left': {
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: 0,
+  },
+  mainLink: {
+    color: '#FFF',
+    textDecoration: 'none',
+    fontSize: 18,
+  },
+  flexGrow: {
+    flexGrow: 1,
+  },
+  logo: {
+    height: 50,
+    marginRight: 23,
+    background: 'white',
+  },
+  userForm: {
+    marginRight: 23,
+  },
+  select: {
+    color: 'white',
+  },
+  option: {
+    color: 'black',
+  },
+  focused: {
+    color: 'white !important',
+  },
+  underline: {
+    '&:before': {
+      borderColor: 'white',
+    },
+  },
+  userLabel: {
+    color: 'white',
+  },
+  selectIcon: {
+    color: 'white',
+  },
+  margin: {
+    margin: theme.spacing.unit * 2,
+  },
+  padding: {
+    padding: `0 ${theme.spacing.unit * 2}px`,
+  },
+  newsButton: {
+    width: 'auto',
+    paddingRight: '20px',
+    borderRadius: 0,
+  },
+  badgeClass: {
+    right: '-25px !important',
+  },
+  badgeContainer: {
+    margin: theme.spacing.unit * 2,
+  },
+  newsMenu: {
+    width: '400px',
+    height: '1000px',
+  },
+  newsItem: {
+    borderBottom: '1px solid #00000021',
+    display: 'block',
+    height: 'auto',
+  },
+  newsName: {
+    float: 'left',
+    clear: 'both',
+  },
+  newsTitle: {
+    whiteSpace: 'normal',
+    lineHeight: '14px',
+    float: 'left',
+    clear: 'both',
+  },
+  newsLink: {
+    color: '#333333',
+    textDecoration: 'none',
+  },
+  formRoot: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing.unit * 3,
+  },
+  formGroup: {
+    margin: `${theme.spacing.unit}px 0`,
+  },
+});
+
+export default styles;
